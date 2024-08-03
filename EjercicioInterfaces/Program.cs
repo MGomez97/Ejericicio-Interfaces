3d8e3bef8217967849edd460cb0c42f05c336dac
MGomez97 <manuelgomez19010@gmail.com>
2024-08-02 21:01:07 -0600

    Update Program.cs

--- EjercicioInterfaces/Program.cs
+++ EjercicioInterfaces/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        OperacionesMatematicas operaciones = new OperacionesMatematicas();
+
+        // Ejemplo de uso
+        Console.WriteLine("Ingrese el largo y el ancho para calcular el área:");
+        double largo = double.Parse(Console.ReadLine());
+        double ancho = double.Parse(Console.ReadLine());
+        double area = operaciones.CalcularArea(largo, ancho);
+        operaciones.Imprimir(area);
+
+        // Aquí puedes seguir pidiendo datos y calculando otras operaciones según lo definido en la interfaz
+    }
+}
